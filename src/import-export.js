f5f093e5223a6e2b8fc39a7e3c3321f194b7ac17
Armin <vanderburgarmin@gmail.com>
2026-07-12 19:58:17 +0200

    Changed some things

--- src/import-export.js
+++ src/import-export.js
@@ -1,17 +1,10 @@
 const exportBtn = document.getElementById('Export');
-const importBtn = document.getElementById('Import');
 const exportContainer = document.getElementById('export-container');
-const importContainer = document.getElementById('import-container');
 const closeButtons = document.querySelectorAll('.close-button');
 
 exportBtn.addEventListener('click', () => {
     exportContainer.style.display = 'flex';
     importContainer.style.display = 'none';
-});
-
-importBtn.addEventListener('click', () => {
-    importContainer.style.display = 'flex';
-    exportContainer.style.display = 'none';
 });
 
 closeButtons.forEach(button => {
